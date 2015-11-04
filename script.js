69ca49f42066a87bea8824b3a028c0299f33b95e
Elana Kopelevich <ekopelevich@gmail.com>
2015-11-04 14:08:51 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+console.log("sanity chack!");
+
+var body = document.getElementsByTagName('body')[0];
+
+var black = document.createElement('div');
+black.style.width = '11.1%';
+black.style.paddingBottom = '11.1%';
+black.style.backgroundColor = 'black';
+black.style.float ='left';
+body.appendChild(black);
+
+for (var i = 0; i < 31; i++) {
+
+  var red = document.createElement('div');
+  red.style.width = '11.1%';
+  red.style.paddingBottom = '11.1%';
+  red.style.backgroundColor = 'red';
+  red.style.float ='left';
+  body.appendChild(red);
+
+  black = document.createElement('div');
+  black.style.width = '11.1%';
+  black.style.paddingBottom = '11.1%';
+  black.style.backgroundColor = 'black';
+  black.style.float ='left';
+  body.appendChild(black);
+}
