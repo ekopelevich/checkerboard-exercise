35d5ad4b01fd186e83952a4da5ebf050221f02b9
Elana Kopelevich <ekopelevich@gmail.com>
2015-11-04 15:31:29 -0700

    checkerboard with random colors

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var body = document.getElementsByTagName('body')[0];
+var color;
 
-for (var i = 0; i < 31; i++) {
-  var color = "#"+((1<<24)*Math.random()|0).toString(16)
+
+for (var i = 0; i < 63; i++) {
+  color = '#'+ (Math.random() * 0xFFFFFF << 0).toString(16);
   var tile = document.createElement('div');
   tile.style.width = '11.1%';
   tile.style.paddingBottom = '11.1%';
-  tile.style.backgroundColor = 'color';
+  tile.style.backgroundColor = color;
   tile.style.float ='left';
   body.appendChild(tile);
 }
