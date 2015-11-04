bca2aebb6666d01f5736fde3a96a7eadc0ebcfad
Elana Kopelevich <ekopelevich@gmail.com>
2015-11-04 15:11:29 -0700

    initial commit on randomcolors branch

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+
+for (var i = 0; i < 31; i++) {
+  var color = "#"+((1<<24)*Math.random()|0).toString(16)
+  var tile = document.createElement('div');
+  tile.style.width = '11.1%';
+  tile.style.paddingBottom = '11.1%';
+  tile.style.backgroundColor = 'color';
+  tile.style.float ='left';
+  body.appendChild(tile);
+}
